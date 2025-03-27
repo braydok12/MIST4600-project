d2309ece50f29c043df19184b8c3a2f58d8a3074
havemercy74 <42220425+havemercy74@users.noreply.github.com>
2025-03-27 01:06:46 -0400

    Update Stock1-1.java

--- Group Project Prototype/Stock1-1.java
+++ Group Project Prototype/Stock1-1.java
@@ -42,35 +42,48 @@ public class Stock {
 		price += price * pct / 100;
 	}
 	public void buy(HashMap<String, Stock> stockDatabase, StockAccount acct) {
-		price = (double)((int)(price * 100)) / 100.0;
-		int inputShares;
-		do {
-			System.out.println("\nYou have " + shares + " shares in " + ticker + ".");
+		if(price < acct.getBalance()) {
+			price = (double)((int)(price * 100)) / 100.0;
+			int inputShares;
+			do {
+				System.out.println("\nYou have " + shares + " shares in " + ticker + ".");
+				System.out.println("The current price of " + ticker + " is $" + price);
+				System.out.println("You have $" + acct.getBalance() + " in your account.");
+				System.out.println("Enter the number of shares to buy:");
+				inputShares = scnr.nextInt();
+			}while((acct.getBalance() - inputShares*price) < 0);
+			shares += inputShares;
+			acct.withdraw(inputShares*price);
+			System.out.println("Successfully bought " + inputShares + " shares for $" + inputShares*price + "!");
+			acct.printAcct(stockDatabase);
+			scnr.nextLine();
+		}else {
 			System.out.println("The current price of " + ticker + " is $" + price);
 			System.out.println("You have $" + acct.getBalance() + " in your account.");
-			System.out.println("Enter the number of shares to buy:");
-			inputShares = scnr.nextInt();
-			scnr.nextLine();
-		}while((acct.getBalance() - inputShares*price) < 0);
-		shares += inputShares;
-		acct.withdraw(inputShares*price);
-		System.out.println("Successfully bought " + inputShares + " shares for $" + inputShares*price + "!");
-		acct.printAcct(stockDatabase);
+			System.out.println("Not enough money to buy a share of " + ticker);
+		}
 	}
 	
-	/*public void buy(int shares) {
-		this.shares += shares;	
-		buyPrice = price;
-	*/
-	public boolean sell(int shares) {
-		// if the amount of shares to sell are more than the current sales,
-		// return false so that another number of shares is input
-		if(shares <= this.shares) {
-			this.shares -= shares;
-			return true;
-		}else {
-			return false;
+	public void sell(HashMap<String, Stock> stockDatabase, StockAccount acct) {
+		price = (double)((int)(price * 100)) / 100.0;
+		int inputShares;
+		System.out.print("\nYou have " + shares + " shares in " + ticker + " to sell. ");
+		System.out.println("The current price of " + ticker + " is $" + price);
+		System.out.println("You have $" + acct.getBalance() + " in your account.");
+		System.out.println("Enter the number of shares to sell:");
+		inputShares = scnr.nextInt();
+		// loops while there are not enough shares to sell
+		// if the user puts a number of shares that is less than or equal to the amount available, the loop breaks
+		while(inputShares > shares) {
+			System.out.println("\nThere are only: " + shares + " shares available to sell.");
+			System.out.println("\nEnter the number of shares to sell:");
+			inputShares = scnr.nextInt();
 		}
+		acct.deposit(inputShares*price);
+		System.out.println("Successfully sold " + inputShares + " shares for $" + inputShares*price + "!");
+		shares -= inputShares;
+		acct.printAcct(stockDatabase);
+		scnr.nextLine();
 	}
 	
 	public static void marketSummary(HashMap<String, Stock> market) {
